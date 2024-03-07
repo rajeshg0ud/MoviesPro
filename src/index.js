9e07e21415456d730756f8d57940f9fc91ecf74b
Rajesh Goud <rajeshgoud2314@gmail.com>
2024-03-07 20:14:46 +0530

    logo & responsive css changes done

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import App from "./App";
 import React from 'react';
 import ReactDOM  from "react-dom/client";
 import './index.css'
-import { Provider, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import appStore from './utils/appStore'
 
 
 const root= ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Provider store={appStore}><App /></Provider>)
+    <Provider store={appStore}>
+            <App />
+    </Provider>)
